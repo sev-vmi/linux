ebd2d5ab1d7ef5fccbf33590c81c987ee24ff4c6
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2021-08-25 21:42:53 +0300

    x86/tdx: Add unaccepted memory support
    Hookup TDX-specific code to accept memory.
    
    Accepting the memory is the same process as converting memory from
    shared to private: kernel notifies VMM with MAP_GPA hypercall and then
    accept pages with ACCEPT_PAGE module call.
    
    The implementation in core kernel uses tdx_enc_status_changed(). It
    already used for converting memory to shared and back for I/O
    transactions.
    
    Boot stub provides own implementation of tdx_accept_memory(). It is
    similar in structure to tdx_enc_status_changed(), but only cares about
    converting memory to private.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- arch/x86/boot/compressed/mem.c
+++ arch/x86/boot/compressed/mem.c
@@ -1,9 +1,42 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
+#include "../cpuflags.h"
+#include "../string.h"
 #include "error.h"
+#include "tdx.h"
+#include <asm/shared/tdx.h>
+
+/*
+ * accept_memory() and process_unaccepted_memory() called from EFI stub which
+ * runs before decompresser and its early_tdx_detect().
+ *
+ * Enumerate TDX directly from the early users.
+ */
+static bool early_is_tdx_guest(void)
+{
+	static bool once;
+	static bool is_tdx;
+
+	if (!IS_ENABLED(CONFIG_INTEL_TDX_GUEST))
+		return false;
+
+	if (!once) {
+		u32 eax, sig[3];
+
+		cpuid_count(TDX_CPUID_LEAF_ID, 0, &eax,
+			    &sig[0], &sig[2],  &sig[1]);
+		is_tdx = !memcmp(TDX_IDENT, sig, sizeof(sig));
+		once = true;
+	}
+
+	return is_tdx;
+}
 
 void arch_accept_memory(phys_addr_t start, phys_addr_t end)
 {
 	/* Platform-specific memory-acceptance call goes here */
-	error("Cannot accept memory");
+	if (early_is_tdx_guest())
+		tdx_accept_memory(start, end);
+	else
+		error("Cannot accept memory: unknown platform\n");
 }
