8d47a46931aa8ff34bd37eb829b31146ac8de4e2
Maxim Levitsky <mlevitsk@redhat.com>
2021-11-08 11:02:45 +0200

    KVM: x86: inhibit APICv when KVM_GUESTDBG_BLOCKIRQ active
    KVM_GUESTDBG_BLOCKIRQ relies on interrupts being injected using
    standard kvm's inject_pending_event, and not via APICv/AVIC.
    
    Since this is a debug feature, just inhibit APICv/AVIC while
    KVM_GUESTDBG_BLOCKIRQ is in use on at least one vCPU.
    
    Fixes: 61e5f69ef0837 ("KVM: x86: implement KVM_GUESTDBG_BLOCKIRQ")
    
    Reported-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Tested-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20211108090245.166408-1-mlevitsk@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -882,6 +882,7 @@ enum kvm_irqchip_mode {
 #define APICV_INHIBIT_REASON_IRQWIN     3
 #define APICV_INHIBIT_REASON_PIT_REINJ  4
 #define APICV_INHIBIT_REASON_X2APIC	5
+#define APICV_INHIBIT_REASON_BLOCKIRQ	6
 
 struct kvm_arch {
 	unsigned long n_used_mmu_pages;
--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -7927,7 +7927,8 @@ static __exit void hardware_unsetup(void)
 static bool vmx_check_apicv_inhibit_reasons(ulong bit)
 {
 	ulong supported = BIT(APICV_INHIBIT_REASON_DISABLE) |
-			  BIT(APICV_INHIBIT_REASON_HYPERV);
+			  BIT(APICV_INHIBIT_REASON_HYPERV) |
+			  BIT(APICV_INHIBIT_REASON_BLOCKIRQ);
 
 	return supported & BIT(bit);
 }
