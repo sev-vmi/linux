ffe56d19a6722320eb42e16bfb1def24693b5cba
Sean Christopherson <seanjc@google.com>
2022-03-11 04:35:15 +0000

    KVM: x86: Make APICv inhibit reasons an enum and cleanup naming
    Use an enum for the APICv inhibit reasons, there is no meaning behind
    their values and they most definitely are not "unsigned longs".  Rename
    the various params to "reason" for consistency and clarity (inhibit may
    be confused as a command, i.e. inhibit APICv, instead of the reason that
    is getting toggled/checked).
    
    No functional change intended.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20220311043517.17027-2-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -7939,14 +7939,14 @@ static __exit void hardware_unsetup(void)
 	free_kvm_area();
 }
 
-static bool vmx_check_apicv_inhibit_reasons(ulong bit)
+static bool vmx_check_apicv_inhibit_reasons(enum kvm_apicv_inhibit reason)
 {
 	ulong supported = BIT(APICV_INHIBIT_REASON_DISABLE) |
 			  BIT(APICV_INHIBIT_REASON_ABSENT) |
 			  BIT(APICV_INHIBIT_REASON_HYPERV) |
 			  BIT(APICV_INHIBIT_REASON_BLOCKIRQ);
 
-	return supported & BIT(bit);
+	return supported & BIT(reason);
 }
 
 static struct kvm_x86_ops vmx_x86_ops __ro_after_init = {
