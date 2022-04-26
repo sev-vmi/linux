89c9248e3fd7b6eab6192d1d4d98302ab1c8e965
Brijesh Singh <brijesh.singh@amd.com>
2022-04-26 18:16:07 +0000

    KVM: SVM: Make AVIC backing, VMSA and VMCB memory allocation SNP safe
    Implement a workaround for an SNP erratum where the CPU will incorrectly
    signal an RMP violation #PF if a hugepage (2mb or 1gb) collides with the
    RMP entry of a VMCB, VMSA or AVIC backing page.
    
    When SEV-SNP is globally enabled, the CPU marks the VMCB, VMSA, and AVIC
    backing   pages as "in-use" in the RMP after a successful VMRUN.  This
    is done for _all_ VMs, not just SNP-Active VMs.
    
    If the hypervisor accesses an in-use page through a writable
    translation, the CPU will throw an RMP violation #PF. On early SNP
    hardware, if an in-use page is 2mb aligned and software accesses any
    part of the associated 2mb region with a hupage, the CPU will
    incorrectly treat the entire 2mb region as in-use and signal a spurious
    RMP violation #PF.
    
    The recommended is to not use the hugepage for the VMCB, VMSA or
    AVIC backing page. Add a generic allocator that will ensure that the
    page returns is not hugepage (2mb or 1gb) and is safe to be used when
    SEV-SNP is enabled.
    
    Co-developed-by: Marc Orr <marcorr@google.com>
    Signed-off-by: Marc Orr <marcorr@google.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -684,6 +684,7 @@ void sev_es_vcpu_reset(struct vcpu_svm *svm);
 void sev_vcpu_deliver_sipi_vector(struct kvm_vcpu *vcpu, u8 vector);
 void sev_es_prepare_switch_to_guest(struct sev_es_save_area *hostsa);
 void sev_es_unmap_ghcb(struct vcpu_svm *svm);
+struct page *snp_safe_alloc_page(struct kvm_vcpu *vcpu);
 
 int sev_fault_is_private(struct kvm *kvm, gpa_t gpa, u64 error_code, bool *private_fault);
 
