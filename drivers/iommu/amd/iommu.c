304274f2a5377d5c263a417743f1b9d3f1006cdd
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2022-10-12 10:30:57 -0500

    iommu/amd: Add support for page response
    This generates AMD IOMMU COMPLETE_PPR_REQUEST for the specified device
    with the specified PRI Response Code.
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Signed-off-by: Wei Huang <wei.huang2@amd.com>
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -2803,6 +2803,7 @@ const struct iommu_ops amd_iommu_ops = {
 	.dev_enable_feat = amd_iommu_dev_enable_feature,
 	.dev_disable_feat = amd_iommu_dev_disable_feature,
 	.remove_dev_pasid = amd_iommu_remove_dev_pasid,
+	.page_response = amd_iommu_page_response,
 	.default_domain_ops = &(const struct iommu_domain_ops) {
 		.attach_dev	= amd_iommu_attach_device,
 		.map_pages	= amd_iommu_map_pages,
