353cc66ebb6eca9fc5defe0f66d5735b67e9b408
Manali Shukla <manali.shukla@amd.com>
2023-02-22 05:35:44 +0000

    KVM: Add KVM_GET_LAPIC_W_EXTAPIC and KVM_SET_LAPIC_W_EXTAPIC for extapic
    There are four additional extended LVT registers available in extended
    APIC register space which can be used for additional interrupt sources
    like instruction based sampling and many more.
    
    Please refer to AMD programmers's manual Volume 2, Section 16.4.5 for
    more details on extapic.
    https://bugzilla.kernel.org/attachment.cgi?id=304653
    
    Adds two new vcpu-based IOCTLs to save and restore the local APIC
    registers with extended APIC register space for a single vcpu. It
    works same as KVM_GET_LAPIC and KVM_SET_LAPIC IOCTLs. The only
    differece is the size of APIC page which is copied/restored by kernel.
    In case of KVM_GET_LAPIC_W_EXTAPIC and KVM_SET_LAPIC_W_EXTAPIC IOCTLs,
    kernel copies/restores the APIC page with extended APIC register space
    located at APIC offsets 400h-530h.
    
    KVM_GET_LAPIC_W_EXTAPIC and KVM_SET_LAPIC_W_EXTAPIC IOCTLs are used
    when extended APIC is enabled in the guest.
    
    Instead of hard coding size of struct kvm_lapic_state_w_extapic,
    kvm_lapic_state_w_extapic was made extensible, so that no need to
    add more UAPIs for future local APIC extensions.
    
    Document KVM_GET_LAPIC_W_EXTAPIC, KVM_SET_LAPIC_W_EXTAPIC ioctls.
    
    Signed-off-by: Manali Shukla <manali.shukla@amd.com>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -2927,7 +2927,7 @@ int kvm_get_apic_interrupt(struct kvm_vcpu *vcpu)
 }
 
 static int kvm_apic_state_fixup(struct kvm_vcpu *vcpu,
-		struct kvm_lapic_state *s, bool set)
+		struct kvm_lapic_state_w_extapic *s, bool set)
 {
 	if (apic_x2apic_mode(vcpu->arch.apic)) {
 		u32 *id = (u32 *)(s->regs + APIC_ID);
@@ -2964,9 +2964,10 @@ static int kvm_apic_state_fixup(struct kvm_vcpu *vcpu,
 	return 0;
 }
 
-int kvm_apic_get_state(struct kvm_vcpu *vcpu, struct kvm_lapic_state *s)
+int kvm_apic_get_state(struct kvm_vcpu *vcpu, struct kvm_lapic_state_w_extapic *s,
+		       unsigned int size)
 {
-	memcpy(s->regs, vcpu->arch.apic->regs, sizeof(*s));
+	memcpy(s->regs, vcpu->arch.apic->regs, size);
 
 	/*
 	 * Get calculated timer current count for remaining timer period (if
@@ -2978,7 +2979,8 @@ int kvm_apic_get_state(struct kvm_vcpu *vcpu, struct kvm_lapic_state *s)
 	return kvm_apic_state_fixup(vcpu, s, false);
 }
 
-int kvm_apic_set_state(struct kvm_vcpu *vcpu, struct kvm_lapic_state *s)
+int kvm_apic_set_state(struct kvm_vcpu *vcpu, struct kvm_lapic_state_w_extapic *s,
+		       unsigned int size)
 {
 	struct kvm_lapic *apic = vcpu->arch.apic;
 	int r;
@@ -2994,7 +2996,7 @@ int kvm_apic_set_state(struct kvm_vcpu *vcpu, struct kvm_lapic_state *s)
 		kvm_recalculate_apic_map(vcpu->kvm);
 		return r;
 	}
-	memcpy(vcpu->arch.apic->regs, s->regs, sizeof(*s));
+	memcpy(vcpu->arch.apic->regs, s->regs, size);
 
 	atomic_set_release(&apic->vcpu->kvm->arch.apic_map_dirty, DIRTY);
 	kvm_recalculate_apic_map(vcpu->kvm);
