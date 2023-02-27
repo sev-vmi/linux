b3d9896f02e34773fe91ff0abc52411c6d4a87f7
Tom Lendacky <thomas.lendacky@amd.com>
2023-02-27 11:32:59 -0600

    x86/sev: Perform early PVALIDATE using the SVSM_CORE_PVALIDATE call
    The PVALIDATE instruction can only be performed at VMPL0. An SVSM will
    be present when running at VMPL1 or lower.
    
    When an SVSM is present, use the SVSM_CORE_PVALIDATE call to perform
    memory validation instead of issuing the PVALIDATE instruction directly.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- arch/x86/include/asm/sev.h
+++ arch/x86/include/asm/sev.h
@@ -156,6 +156,26 @@ struct svsm_caa {
 	u8 svsm_buffer[PAGE_SIZE - 8];
 };
 
+/*
+ * The SVSM PVALIDATE related structures
+ */
+struct svsm_pvalidate_entry {
+	u64 page_size		: 2,
+	    action		: 1,
+	    ignore_cf		: 1,
+	    rsvd		: 8,
+	    pfn			: 52;
+};
+
+struct svsm_pvalidate_call {
+	u16 entries;
+	u16 next;
+
+	u8 rsvd1[4];
+
+	struct svsm_pvalidate_entry entry[];
+};
+
 #ifdef CONFIG_AMD_MEM_ENCRYPT
 extern struct static_key_false sev_es_enable_key;
 extern void __sev_es_ist_enter(struct pt_regs *regs);
