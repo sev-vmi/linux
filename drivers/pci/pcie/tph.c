1eba3b97d34949b679007b1c5b7671d35003ba9a
Eric Van Tassell <Eric.VanTassell@amd.com>
2023-03-04 14:04:05 -0600

    [PCI] Complete implementation of invoke_dsm()
    Call the _DSM firmware method which for a given cpu id
    returns 4 valid bits and 4 tag values for each type of memory:
    {volatile, non-volatile} X tag: {normal, extended}
    
    Co-developed-by: Wei Huang <wei.huang2@amd.com>
    Signed-off-by: Wei Huang <wei.huang2@amd.com>
    Signed-off-by: Eric Van Tassell <Eric.VanTassell@amd.com>

--- drivers/pci/pcie/tph.c
+++ drivers/pci/pcie/tph.c
@@ -6,12 +6,15 @@
  * Author: Eric Van Tassell (Eric.VanTassell@amd.com)
  */
 
+#include <linux/acpi.h>
 #include <uapi/linux/pci_regs.h>
+#include <linux/acpi.h>
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/msi.h>
 #include <linux/pci.h>
 #include <linux/msi.h>
+#include <linux/pci-acpi.h>
 
 #ifdef CONFIG_PCIE_TPH
 
@@ -205,6 +208,101 @@ static enum st_table_location tph_get_table_location(struct pci_dev *dev,
 	return 0;
 }
 
+/*
+ * invoke_dsm - invoke the firmware method to retrieve the steering tags
+ * for a core
+ * @handle: ACPI handle of the device where the firmwre method is
+ * implemented
+ * @cpu_uid: ACPI processor UID as specified in MADT
+ * @processor_hint:
+ *         00b: bi directional data structure - equal read/write access by
+ *              host and device
+ *         01b: device writes and reads, reads and writes again soon
+ *         10b: device writes and host soon reads _or_ host writes and device
+ *              soon reads
+ *         11b: same as 10b but with temporal re-use priority
+ *
+ * @target_type: 0 => a processor, 1 => processor container
+ * @cache_reference_valid: indicates that the cache_reference parameter is valid
+ * @cache_reference: Cache ID of the specific cache
+ * @st_tag_out: Where to write the steering tag acquired from the _DSM method.
+ *
+ * Call the firmware _DSM method to get the steering tag an endpoint device can
+ * use to target the cache of a the core on a PCIe write transaction.
+ *
+ * Return
+ *        true: contents of *st_tag_out are valid
+ *        false: contents of *st_tag_out are invalid
+ */
+
+#define MINIMUM_DSM_REVISION		7
+#define DSM_STEERING_TAG_FUNCTION_INDEX	0xf
+
+static bool invoke_dsm(acpi_handle handle, u32 cpu_uid, u8 processor_hint,
+		       u8 target_type, bool cache_reference_valid,
+		       u64 cache_reference, union st_info *st_tag_out)
+{
+	union acpi_object in_obj, in_buf[3], *out_obj;
+
+	in_buf[0].integer.type = ACPI_TYPE_INTEGER;
+	in_buf[0].integer.value = 0; /* 0 => processor cache steering tags */
+
+	in_buf[1].integer.type = ACPI_TYPE_INTEGER;
+	in_buf[1].integer.value = cpu_uid;
+
+	in_buf[2].integer.type = ACPI_TYPE_INTEGER;
+	in_buf[2].integer.value = processor_hint & 3;
+	in_buf[2].integer.value |= (target_type & 1) << 2;
+	in_buf[2].integer.value |= (cache_reference_valid & 1) << 3;
+	in_buf[2].integer.value |= (cache_reference << 32);
+
+	in_obj.type = ACPI_TYPE_PACKAGE;
+	in_obj.package.count = ARRAY_SIZE(in_buf);
+	in_obj.package.elements = in_buf;
+
+	out_obj = acpi_evaluate_dsm(handle,
+				    &pci_acpi_dsm_guid, MINIMUM_DSM_REVISION,
+				    DSM_STEERING_TAG_FUNCTION_INDEX, &in_obj);
+
+	if (!out_obj) {
+		pr_err("%s: acpi_evaluate_dsm() FAIL\n", __func__);
+		return false;
+	}
+
+	if (out_obj->type != ACPI_TYPE_BUFFER) {
+		pr_err("%s: acpi_evaluate_dsm(): invalid return type %d\n",
+		       __func__, out_obj->type);
+		return false;
+	}
+	st_tag_out->value = *((u64 *)(out_obj->buffer.pointer));
+	ACPI_FREE(out_obj);
+
+	return true;
+}
+
+static acpi_handle root_complex_acpi_handle(struct pci_dev *dev)
+{
+	struct pci_dev *root_port;
+
+	root_port = pcie_find_root_port(dev);
+	if (!root_port || !root_port->bus || !root_port->bus->bridge) {
+		WARN_ONCE(1, "cannot find root port for pci_dev\n");
+		return NULL;
+	}
+	return ACPI_HANDLE(root_port->bus->bridge);
+}
+
+/*
+ * Steering tags always occupy 16 bits but, if the device only supports,
+ * 8 bit tags, the upper 8 bits are 0.
+ */
+static u16 tph_extract_tag(enum tph_mtype_tag tag_type,
+			   enum tph_requester_enable req_enable,
+			   union st_info *st_tag_out)
+{
+	return 0;
+}
+
 /**
  * pcie_tph_read_steering_tag() - get steering tag table entry
  * @cpu: the acpi cpuuid.
@@ -226,7 +324,17 @@ static u16 pcie_tph_read_steering_tag(struct pci_dev *dev, unsigned int cpu,
 				      enum tph_mtype_tag tag_type,
 				      enum tph_requester_enable req_enable)
 {
-	return 0; /* dont steer */
+	union st_info st_tag_out;
+	u16 tagval;
+
+	if (!invoke_dsm(root_complex_acpi_handle(dev), cpu, 0, 0, false, 0,
+			&st_tag_out)) {
+		WARN_ONCE(1, "_DSM did not return valid steering tag\n");
+		tagval = 0; /* 0 means "no steering" */
+	} else {
+		tagval = tph_extract_tag(tag_type, req_enable, &st_tag_out);
+	}
+	return tagval;
 }
 
 /**
