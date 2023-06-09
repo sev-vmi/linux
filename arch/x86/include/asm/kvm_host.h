08502bf48fbf4aa2dc5c57bf80302d38091e0ba5
Michael Roth <michael.roth@amd.com>
2023-06-09 08:53:37 -0500

    KVM: x86: Add gmem hook for invalidating memory
    In some cases, like with SEV-SNP, guest memory needs to be updated in a
    platform-specific manner before it can be safely freed back to the host.
    Wire up arch-defined hooks to the .free_folio kvm_gmem_aops callback to
    allow for special handling of this sort when freeing memory in response
    to FALLOC_FL_PUNCH_HOLE operations and when releasing the inode, and go
    ahead and define an arch-specific hook for x86 since it will be needed
    for handling memory used for SEV-SNP guests.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1754,6 +1754,7 @@ struct kvm_x86_ops {
 	unsigned long (*vcpu_get_apicv_inhibit_reasons)(struct kvm_vcpu *vcpu);
 
 	int (*gmem_prepare)(struct kvm *kvm, kvm_pfn_t pfn, gfn_t gfn, int max_order);
+	void (*gmem_invalidate)(kvm_pfn_t start, kvm_pfn_t end);
 };
 
 struct kvm_x86_nested_ops {
