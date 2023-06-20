27c7b115a2a81329c99634779c0d7601bcba48bd
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2023-06-20 13:57:30 -0500

    iommufd: Introduce AMD HW-vIOMMU IOCTL
    Add support for AMD HW-vIOMMU in the iommufd /dev/iommu devfs.
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

---
+++ include/linux/amd-viommu.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2022 Advanced Micro Devices, Inc.
+ */
+
+#ifndef _LINUX_AMD_VIOMMU_H
+#define _LINUX_AMD_VIOMMU_H
+
+#include <uapi/linux/amd_viommu.h>
+
+extern long iommufd_amd_viommu_ioctl(struct file *filp,
+				     unsigned int cmd,
+				     unsigned long arg);
+
+extern long iommufd_viommu_ioctl(struct file *filp, unsigned int cmd,
+			  unsigned long arg);
+
+extern int amd_viommu_iommu_init(struct amd_viommu_iommu_info *data);
+extern int amd_viommu_iommu_destroy(struct amd_viommu_iommu_info *data);
+extern int amd_viommu_device_update(struct amd_viommu_dev_info *data, bool is_set);
+extern int amd_viommu_guest_mmio_write(struct amd_viommu_mmio_data *data);
+extern int amd_viommu_guest_mmio_read(struct amd_viommu_mmio_data *data);
+
+#endif /* _LINUX_AMD_VIOMMU_H */
