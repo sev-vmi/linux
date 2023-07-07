cd71a5fc38faea02a1a77cf44ddf3b218aa68c7e
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2023-07-07 01:22:09 -0500

    iommu/amd: Introduce get_amd_iommu_from_dev()
    And replace rlookup_amd_iommu() with the new helper function where
    applicable to avoid unnecessary loop to look up struct amd_iommu from
    struct device.
    
    Suggested-by: Jason Gunthorpe <jgg@ziepe.ca>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>

--- drivers/iommu/amd/amd_iommu.h
+++ drivers/iommu/amd/amd_iommu.h
@@ -150,6 +150,17 @@ static inline void *alloc_pgtable_page(int nid, gfp_t gfp)
 	return page ? page_address(page) : NULL;
 }
 
+/*
+ * This must be called after device probe completes. During probe
+ * use rlookup_amd_iommu() get the iommu.
+ */
+static inline struct amd_iommu *get_amd_iommu_from_dev(struct device *dev)
+{
+	struct iommu_device *iommu = iommu_get_iommu_dev(dev);
+
+	return container_of(iommu, struct amd_iommu, iommu);
+}
+
 bool translation_pre_enabled(struct amd_iommu *iommu);
 bool amd_iommu_is_attach_deferred(struct device *dev);
 int __init add_special_device(u8 type, u8 id, u32 *devid, bool cmd_line);
