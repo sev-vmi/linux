ec25de0e714129e3e00d7467abeae3584027f03c
Carlos Bilbao <carlos.bilbao@amd.com>
2023-07-10 18:01:53 -0500

    KVM: SVM: Support launching an SVSM with Restricted Injection set
    Allow an Restricted Injection to be set in SEV_FEATURES. When set,
    attempts to inject any interrupts other than #HV will make VMRUN fail.
    This is done to further reduce the security exposure within the SVSM.
    
    Signed-off-by: Carlos Bilbao <carlos.bilbao@amd.com>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -692,9 +692,13 @@ static int sev_es_sync_vmsa(struct vcpu_svm *svm)
 	save->xss  = svm->vcpu.arch.ia32_xss;
 	save->dr6  = svm->vcpu.arch.dr6;
 
-	/* Enable the SEV-SNP feature */
-	if (sev_snp_guest(svm->vcpu.kvm))
+	/* Enable the SEV-SNP features */
+	if (sev_snp_guest(svm->vcpu.kvm)) {
 		save->sev_features |= SVM_SEV_FEAT_SNP_ACTIVE;
+
+		if (has_snp_feature(sev, KVM_SEV_SNP_RESTRICTED_INJET))
+			save->sev_features |= SVM_SEV_FEAT_RESTRICTED_INJECTION;
+	}
 
 	/*
 	 * Save the VMSA synced SEV features. For now, they are the same for
