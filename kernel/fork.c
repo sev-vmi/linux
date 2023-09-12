8e035b17f750352a901a8b3bcf21e921c83e9734
Tina Zhang <tina.zhang@intel.com>
2023-09-12 20:59:35 +0800

    iommu: Support mm PASID 1:n with sva domains
    Each mm bound to devices gets a PASID and corresponding sva domains
    allocated in iommu_sva_bind_device(), which are referenced by iommu_mm
    field of the mm. The PASID is released in __mmdrop(), while a sva domain
    is released when no one is using it (the reference count is decremented
    in iommu_sva_unbind_device()). However, although sva domains and their
    PASID are separate objects such that their own life cycles could be
    handled independently, an enqcmd use case may require releasing the
    PASID in releasing the mm (i.e., once a PASID is allocated for a mm, it
    will be permanently used by the mm and won't be released until the end
    of mm) and only allows to drop the PASID after the sva domains are
    released. To this end, mmgrab() is called in iommu_sva_domain_alloc() to
    increment the mm reference count and mmdrop() is invoked in
    iommu_domain_free() to decrement the mm reference count.
    
    Since the required info of PASID and sva domains is kept in struct
    iommu_mm_data of a mm, use mm->iommu_mm field instead of the old pasid
    field in mm struct. The sva domain list is protected by iommu_sva_lock.
    
    Besides, this patch removes mm_pasid_init(), as with the introduced
    iommu_mm structure, initializing mm pasid in mm_init() is unnecessary.
    
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>
    Signed-off-by: Tina Zhang <tina.zhang@intel.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1277,7 +1277,6 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 	mm_init_cpumask(mm);
 	mm_init_aio(mm);
 	mm_init_owner(mm, p);
-	mm_pasid_init(mm);
 	RCU_INIT_POINTER(mm->exe_file, NULL);
 	mmu_notifier_subscriptions_init(mm);
 	init_tlb_flush_pending(mm);
