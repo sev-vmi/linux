46d716d73ae3539f95436de451043ff2809904be
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2023-09-24 22:05:57 -0500

    iommu/amd: Check hardware capabilities and enable vIOMMU support
    The AMD vIOMMU support requires VCMD, VIOMMU, GCR3TRPMODE, and
    GstBufferTRPMode capabilities in the hardware to support the feature.
    
    Add logic to check and enable the required capabilities.
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- drivers/iommu/amd/viommu.c
+++ drivers/iommu/amd/viommu.c
@@ -40,6 +40,22 @@ struct amd_iommu *get_amd_iommu_from_devid(u16 devid)
 	return NULL;
 }
 
+static int viommu_enable(struct amd_iommu *iommu)
+{
+	if (!amd_iommu_viommu)
+		return -EINVAL;
+
+	/* The GstBufferTRPMode feature is checked by set and test */
+	if (!iommu_feature_enable_and_check(iommu, CONTROL_GSTBUFFERTRPMODE))
+		return -EINVAL;
+
+	iommu_feature_enable(iommu, CONTROL_GCR3TRPMODE);
+	iommu_feature_enable(iommu, CONTROL_VCMD_EN);
+	iommu_feature_enable(iommu, CONTROL_VIOMMU_EN);
+
+	return 0;
+}
+
 static int viommu_init_pci_vsc(struct amd_iommu *iommu)
 {
 	iommu->vsc_offset = pci_find_capability(iommu->dev, PCI_CAP_ID_VNDR);
@@ -217,6 +233,10 @@ int __init iommu_init_viommu(struct amd_iommu *iommu)
 	if (!amd_iommu_viommu)
 		return 0;
 
+	if (!check_feature(FEATURE_VIOMMU) ||
+	    !check_feature2(FEATURE_GCR3TRPMODESUP))
+		goto err_out;
+
 	ret = viommu_init_pci_vsc(iommu);
 	if (ret)
 		goto err_out;
@@ -226,6 +246,10 @@ int __init iommu_init_viommu(struct amd_iommu *iommu)
 		goto err_out;
 
 	ret = viommu_private_space_init(iommu);
+	if (ret)
+		goto err_out;
+
+	ret = viommu_enable(iommu);
 	if (ret)
 		goto err_out;
 
