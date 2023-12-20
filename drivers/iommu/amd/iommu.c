9090353753af1c16f78e1d681d8bd83261033cc7
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2023-12-20 12:10:50 +0530

    iommu/amd: Introduce logic to enable/disable IOPF
    Add support to enable/disable IOPF feature. So that PRI capable device
    can make use of page fault handler in SVA mode.
    
    Note that enable_feature(IOPF) just returns success, as this interface
    is going away. IOPF handler will be setup in PASID bind path. Following
    patch will add this support.
    
    Also add IOMMU_IOPF as dependency to AMD_IOMMU driver.
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Co-developed-by: Vasant Hegde <vasant.hegde@amd.com>
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -2782,9 +2782,11 @@ const struct iommu_dirty_ops amd_dirty_ops = {
 static int amd_iommu_dev_enable_feature(struct device *dev,
 					enum iommu_dev_features feat)
 {
-	int ret;
+	int ret = 0;
 
 	switch (feat) {
+	case IOMMU_DEV_FEAT_IOPF:
+		break;
 	default:
 		ret = -EINVAL;
 		break;
@@ -2795,9 +2797,11 @@ static int amd_iommu_dev_enable_feature(struct device *dev,
 static int amd_iommu_dev_disable_feature(struct device *dev,
 					 enum iommu_dev_features feat)
 {
-	int ret;
+	int ret = 0;
 
 	switch (feat) {
+	case IOMMU_DEV_FEAT_IOPF:
+		break;
 	default:
 		ret = -EINVAL;
 		break;
