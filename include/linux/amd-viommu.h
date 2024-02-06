15c6567f7c8165a53a1bbf29d39a08c233e69d12
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2024-02-06 08:42:18 -0600

    iommu/amd: Add Support for IOMMU Command Buffer Virtualization w/o GSTBufferTRPMode
    In this case, when the guest sets up the guest command buffer
    by programming the GPA of the buffer to the MMIO Offset 0008h
    (i.e. Command Buffer Base Address Register), the hypervisor traps
    the MMIO write, extract the GPA/HVA of the buffer, and pins and map
    the pages in the IOMMU v1 table with the IOMMU Private Address (IPA).
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- include/linux/amd-viommu.h
+++ include/linux/amd-viommu.h
@@ -20,5 +20,6 @@ extern int amd_viommu_iommu_destroy(struct amd_viommu_iommu_info *data);
 extern int amd_viommu_device_update(struct amd_viommu_dev_info *data, bool is_set);
 extern int amd_viommu_guest_mmio_write(struct amd_viommu_mmio_data *data);
 extern int amd_viommu_guest_mmio_read(struct amd_viommu_mmio_data *data);
+extern int amd_viommu_cmdbuf_update(struct amd_viommu_cmdbuf_data *data);
 
 #endif /* _LINUX_AMD_VIOMMU_H */
